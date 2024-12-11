72edda73b83cf1ea1c92e79a1974672d5720c5b7
lattiric <78038846+lattiric@users.noreply.github.com>
2024-12-11 03:36:06 -0600

    added multiple story frame functionality

---
+++ StoryScape/StoryScape/SpeechModel.swift
@@ -0,0 +1,12 @@
+//
+//  SpeechModel.swift
+//  StoryScape
+//
+//  Created by Rick Lattin on 12/11/24.
+//
+
+import UIKit
+
+class SpeechModel: NSObject {
+
+}
---
+++ StoryScape/StoryScape/StoryFrameModel.swift
@@ -0,0 +1,12 @@
+//
+//  StoryFrameModel.swift
+//  StoryScape
+//
+//  Created by Rick Lattin on 12/11/24.
+//
+
+import UIKit
+
+class StoryFrameModel: NSObject {
+
+}
